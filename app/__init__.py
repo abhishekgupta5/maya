3fec784fcd7bea365588461d80b657269c3aa1aa
Abhishek Gupta <abhishekmodi2011@gmail.com>
2018-04-13 16:49:32 +0530

    Database connections and migrations functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
-# app/__init__.py
+#app/__init__.py
 
+#3rd party Imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+#Local imports
 from config import app_config
+
+#Initializing db object
+db = SQLAlchemy()
 
 def create_app(config_name):
     #App config details
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-
+    #For dev only
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    #Initializing database
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    from app import models
     @app.route('/')
     def test_server():
         return 'Testing flask'
---
+++ app/models.py
@@ -0,0 +1,16 @@
+#app/models.py
+
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    #For testing migrations
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        #self.password = password
+
