37299c80009f9727dd8bd93164182481264449a6
Abhishek Gupta <abhishekmodi2011@gmail.com>
2018-04-14 19:23:55 +0530

    basic user model and auth blueprint

--- app/models.py
+++ app/models.py
@@ -1,16 +1,24 @@
 #app/models.py
 
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 class User(db.Model):
+    '''
+    Table for users.
+    '''
     __tablename__ = 'users'
 
-    #For testing migrations
+    #Columns
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True, index=True)
+    #Temporary storing passwords without hashing
+    password_plain = db.Column(db.String(128), nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
-        #self.password = password
+    def __init__(self, email, password_plain):
+        self.email = email
+        self.password_plain = password_plain
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.name)
 
